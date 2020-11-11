8e3c7314df3e56bc9461f25e5a17b8b62052a5bf
jwnukoski <jwnukoski@protonmail.com>
2020-11-11 15:44:10 -0500

    added app routes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const database = require('./database.js');
 const conn = require('../connection.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static(path.join(__dirname, '/../public')));
+
+app.get('/api/getrelatedpurchases', (req, res) => {
+  /*return dbfunction.then((results) => {
+    res.send(results);
+  });*/
+});
+
+app.post('/api/addrelatedpurchase', (req, res) => {
+  /*return dbfunction.then((results) => {
+    res.send(results);
+  });*/
+});
+
+app.post('/api/deleterelatedpurchase', (req, res) => {
+  /*return dbfunction.then((results) => {
+    res.send(results);
+  });*/
+});
+
+app.get('/api/getdetails', (req, res) => {
+  /*return dbfunction.then((results) => {
+    res.send(results);
+  });*/
+});
+
+app.post('/api/adddetails', (req, res) => {
+  /*return dbfunction.then((results) => {
+    res.send(results);
+  });*/
+});
 
 app.listen(conn.port, () => {
   console.log(`App listening on ${conn.port}`)
