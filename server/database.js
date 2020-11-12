c8428d9d0c0def897ce30493b95f43480d1bf781
Joe Wnukoski <59515951+jwnukoski@users.noreply.github.com>
2020-11-12 15:10:38 -0500

    Api calls (#4)
    * example test
    
    * added node_modules
    
    * mocha test suite
    
    * This commit adds the seeder script, for populating the database with fake data.
    It relies on promises due to there being referenced IDs in other tables.
    
    A connection.js file has been added for ease of use with getting the SQL connection information.
    
    Packages installed:
    - mysql (for db connection queries)
    - faker (for fake date)
    
    * Removed commits for testing, which made it to this branch. I will move those over to the testing branch.
    
    * added random image to items table
    
    * cleaned up comments
    
    * cleaned up comments
    
    * Base packages for runnning react (webpack compiled) client, and express server.
    
    * added app routes
    
    * getrelatedpurchases working
    
    * moved nodemon
    
    * Moved nodemon as dev dependency. Minor changes in scripts.
    
    * mapped out rest of post and get requests
    
    * resolved seeder conflicts

---
+++ server/database.js
@@ -0,0 +1,27 @@
+const conn = require('../connection.js');
+
+module.exports.getRelatedPurchases = function getRelatedPurchases(itemId, succ, err) {
+  conn.dbConn.query('SELECT i.details_id,i.description,i.title,i.cost,i.rating,i.image_url FROM related r INNER JOIN items i ON r.item_id = i.id WHERE parent_item_id = ?', [itemId],  (error, results) => {
+    if (error) {
+      err(error);
+    } else {
+      succ(results);
+    }
+  });
+};
+
+module.exports.addRelatedPurchase = function addRelatedPurchase() {
+
+};
+
+module.exports.deleteRelatedPurchase = function deleteRelatedPurchase() {
+
+};
+
+module.exports.getDetails = function getDetails() {
+
+};
+
+module.exports.addDetails = function addDetails() {
+
+};
