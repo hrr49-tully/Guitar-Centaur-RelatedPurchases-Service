c97ee2788c88d38092baa5520828f067e5770130
jwnukoski <jwnukoski@protonmail.com>
2020-11-12 22:56:54 -0500

    related endpoint tests

--- tests/endpoints.js
+++ tests/endpoints.js
@@ -15,3 +15,18 @@ describe('/api/getRelatedPurchases1 end point:', () => {
     }).end(done);
   });
 });
+
+describe('/api/addrelatedpurchase end point:', () => {
+  it('Should post data when given a pid and iid:', (done) => {
+    request.post('/api/addrelatedpurchase?pid=1&iid=1').expect(200).expect((res) => {
+      expect(res.text).toBeDefined();
+      expect(res.status).toEqual(200);
+    }).end(done);
+  });
+
+  it('Should fail when not given enough information:', (done) => {
+    request.post('/api/addrelatedpurchase').expect(401).expect((res) => {
+      expect(res.status).toEqual(401);
+    }).end(done);
+  });
+});
