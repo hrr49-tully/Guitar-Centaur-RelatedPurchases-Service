f297525211695e708f48815581b5a549fe082ac3
jwnukoski <jwnukoski@protonmail.com>
2020-11-16 12:49:50 -0500

    getdetails

--- tests/endpoints.test.js
+++ tests/endpoints.test.js
@@ -16,6 +16,35 @@ describe('/api/getRelatedPurchases end point:', () => {
   });
 });
 
+describe('/api/getdetails end point:', () => {
+  it('Should fetch overview details for an item when given an ID', (done) => {
+    request.get('/api/getdetails?id=1').expect(200).expect((res) => {
+      expect(JSON.parse(res.text)[0].overview).toBeDefined();
+      expect(res.status).toEqual(200);
+    }).end(done);
+  });
+
+  it('Should fetch specification details for an item when given an ID', (done) => {
+    request.get('/api/getdetails?id=1').expect(200).expect((res) => {
+      expect(JSON.parse(res.text)[0].specifications).toBeDefined();
+      expect(res.status).toEqual(200);
+    }).end(done);
+  });
+
+  it('Should fetch coverage details for an item when given an ID', (done) => {
+    request.get('/api/getdetails?id=1').expect(200).expect((res) => {
+      expect(JSON.parse(res.text)[0].coverage).toBeDefined();
+      expect(res.status).toEqual(200);
+    }).end(done);
+  });
+
+  it('Should fail when not given an ID, and retrurn a 401.', (done) => {
+    request.get('/api/getdetails').expect(401).expect((res) => {
+      expect(res.status).toEqual(401);
+    }).end(done);
+  });
+});
+
 describe('/api/addrelatedpurchase end point:', () => {
   it('Should post data when given a pid and iid', (done) => {
     request.post('/api/addrelatedpurchase?pid=1&iid=1').expect(200).expect((res) => {
