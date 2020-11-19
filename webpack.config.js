4ec16ccc0797702299f5e32378a698e144214f3a
jwnukoski <jwnukoski@protonmail.com>
2020-11-19 02:12:22 -0500

    fixed static url in ratings star image

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,11 @@ module.exports = {
       }
     ]
   },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
