728a53c13043ec53eb54096b0b047dbe9681bf14
jwnukoski <jwnukoski@protonmail.com>
2020-11-21 13:31:01 -0500

    item_id check

--- client/App.jsx
+++ client/App.jsx
@@ -32,7 +32,9 @@ class App extends React.Component {
     item_id.shift();
     item_id = item_id.join('');
 
-    this.handleItemChange(item_id);
+    if (item_id >= 0) {
+      this.handleItemChange(item_id);
+    }
   }
 
   handleItemChange(id) {
