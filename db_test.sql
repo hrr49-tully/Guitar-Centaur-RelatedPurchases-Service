e052cae5de650baed2c4d18cc61707c2173e3a6b
jwnukoski <jwnukoski@protonmail.com>
2020-11-24 10:36:29 -0500

    debug password

--- db_test.sql
+++ db_test.sql
@@ -3,5 +3,5 @@ CREATE USER 'student'@'%' IDENTIFIED BY 'password';
 ALTER USER 'student'@'%' IDENTIFIED BY 'password';
 GRANT ALL on *.* to student;
 FLUSH PRIVILEGES;
-SET PASSWORD FOR 'student'@'%' = PASSWORD('password');
+SET PASSWORD FOR 'student'@'%' = OLD_PASSWORD('password');
 FLUSH PRIVILEGES;
