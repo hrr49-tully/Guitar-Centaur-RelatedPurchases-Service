1ab40c0830d02d4e72caddc10bb616a4ac1f6d3c
jwnukoski <jwnukoski@protonmail.com>
2020-11-24 09:48:58 -0500

    db test

---
+++ db_test.sql
@@ -0,0 +1,7 @@
+SET old_passwords=0;
+CREATE USER 'student'@'%' IDENTIFIED BY 'password';
+ALTER USER 'student'@'%' IDENTIFIED BY 'password';
+GRANT ALL on *.* to student;
+FLUSH PRIVILEGES;
+SET PASSWORD FOR 'student'@'localhost' = PASSWORD('password');
+FLUSH PRIVILEGES;
